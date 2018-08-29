d6a613dfab9696a8e3a3c61ee94b4eac7ffa37be
trpgd74 <paul_grech@troweprice.com>
2018-08-29 10:52:26 -0400

    Exericse 6

--- src/ex6PromissoryNote/PromissoryNote.java
+++ src/ex6PromissoryNote/PromissoryNote.java
@@ -1,4 +1,22 @@
 package ex6PromissoryNote;
 
+import java.util.HashMap;
+
 public class PromissoryNote {
+
+    private HashMap<String, Double> loans = new HashMap<>();
+
+
+    public void setLoan (String toWhom, Double value){
+        this.loans.put(toWhom, value);
+    }
+
+    public Double howMuchIsTheDebt (String whose) {
+        Double debt = this.loans.get(whose);
+        if (debt != null) {
+            return debt;
+        } else {
+            return 0.0;
+        }
+    }
 }
--- src/ex6PromissoryNote/ex6PromissoryNote.java
+++ src/ex6PromissoryNote/ex6PromissoryNote.java
@@ -1,4 +1,20 @@
 package ex6PromissoryNote;
 
 public class ex6PromissoryNote {
+    public static void main (String [] args) {
+        System.out.println("Promissory Note!");
+
+        PromissoryNote mattisNote = new PromissoryNote();
+        mattisNote.setLoan("Arto", 51.5);
+        mattisNote.setLoan("Mikael", 30.0);
+
+        System.out.println(mattisNote.howMuchIsTheDebt("Arto"));
+        System.out.println(mattisNote.howMuchIsTheDebt("Joel"));
+
+        mattisNote.setLoan("Arto", 10.5);
+
+        System.out.println(mattisNote.howMuchIsTheDebt("Arto"));
+
+        System.out.println(mattisNote.toString());
+    }
 }
