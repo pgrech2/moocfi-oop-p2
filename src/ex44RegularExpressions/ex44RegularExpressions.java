ee817b247520fdca94c03c6bc8007f65089c74aa
pgrech2 <pgrech2@gmail.com>
2018-10-25 13:24:46 -0400

    Exercise 44

--- src/ex44RegularExpressions/ex44RegularExpressions.java
+++ src/ex44RegularExpressions/ex44RegularExpressions.java
@@ -1,4 +1,28 @@
 package ex44RegularExpressions;
 
+import java.util.Scanner;
+
 public class ex44RegularExpressions {
+
+    public static void main (String [] args) {
+        System.out.println("hello world");
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Write a verb: ");
+        String verb = reader.nextLine();
+        System.out.print("\nWrite a form of " + verb + ": ");
+        String verbForm = reader.nextLine();
+        System.out.println(verbForm.matches(verb + "(|s|ed|ing|er)"));
+
+        System.out.println("All vowels: " + isAWeekDay("MOnday"));
+        System.out.println("All vowels: " + allVowels("aezi"));
+    }
+
+    private static boolean isAWeekDay(String string) {
+        return string.toLowerCase().matches("(mon|tues|wed|thurs|fri)(.*)");
+    }
+
+    public static boolean allVowels(String string) {
+        return string.toLowerCase().matches("[aeiou]{1,}");
+    }
 }
